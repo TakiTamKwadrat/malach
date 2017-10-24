9938e3f599fb715f32e4383500929bcff144c267
FridenotChlodulf <Fridenot@mail.com>
2017-10-24 18:19:16 +0200

    Ulamki
    Dodanie klasy Ulamek
    getery,setery,dodawnei liczby do ulamka
    dodawanie ulamkow
    bez testow

---
+++ Ulamki/Main.java
@@ -0,0 +1,11 @@
+public class Main
+{
+  public static void main(String[] args)
+  {
+    Ulamek u = new Ulamek(10,5);
+    u = u.dodaj(3);
+    u.wypisz();
+    u.uprosc();
+    u.wypisz();
+  }
+}
---
+++ Ulamki/Ulamek.java
@@ -0,0 +1,104 @@
+public class Ulamek
+{
+  private int up;
+  private int down;
+
+  private int NWD(int a,int b)
+  {
+    while(b!=0)
+    {
+      int c = a%b;
+      a = b;
+      b = c;
+    }
+    return a;
+  }
+
+  private int NWW(int a,int b)
+  {
+    return a*b/NWD(a,b);
+  }
+
+  public Ulamek()
+  {
+    up = 1;
+    down = 1;
+  }
+
+  public Ulamek(int u,int d)
+  {
+    up = u;
+    down = d;
+  }
+
+  public int getUp()
+  {
+    return up;
+  }
+
+  public int getDown()
+  {
+    return down;
+  }
+
+  public void setUp(int u)
+  {
+    up = u;
+  }
+
+  public void setDown(int d)
+  {
+    down = d;
+  }
+
+  public void uprosc() {
+    int nwd = NWD(up,down);
+    up/=nwd;
+    down/=nwd;
+  }
+
+  public Ulamek dodaj(int a)
+  {
+    return new Ulamek(up+down*a,down);
+  }
+
+  public Ulamek dodaj(Ulamek u)
+  {
+    int nww = NWW(down,u.getDown());
+    Ulamek wynik = new Ulamek(1,nww);
+    int x = nww/down;
+    int x1 = nww/u.getDown();
+    wynik.setUp(up * x + u.getUp() * x1);
+    wynik.uprosc();
+    return wynik;
+  }
+
+  public Ulamek pomnuz(int a)
+  {
+    return new Ulamek(up*a,down);
+  }
+
+  public Ulamek pomnuz(Ulamek u)
+  {
+    return new Ulamek(up*u.getUp(),down*u.getDown());
+  }
+
+  public void wypisz()
+  {
+    int max;
+    if(up>down){
+      max = up;
+    }
+    else {
+      max = down;
+    }
+    int len = String.valueOf(max).length();
+    System.out.println(up);
+    for(int i=0;i<len;i++)
+      System.out.print("-");
+    System.out.println();
+    System.out.println(down);
+
+  }
+
+}
