e29cfafa38fd39f11e4845feac7d473cd5ee9897
FridenotChlodulf <Fridenot@mail.com>
2017-11-03 15:32:59 +0100

    Input2

--- Pobieranie Danych/Main.java
+++ Pobieranie Danych/Main.java
@@ -5,17 +5,18 @@ public class Main
   public static void main(String[] args)
   {
     Scanner in = new Scanner(System.in);
+
     int i = in.nextInt();
     double d = in.nextDouble();
     short s = in.nextShort();
-    //String str = in.nextLine(); <- moze pobierac cale zdanie ale potrzebna jes petla
-    String str1 = in.next();//<- pobiera pojedyncze slowo
+    String str = in.next();//<- pobiera pojedyncze slowo
+    //String str = in.nextLine(); <- moze pobierac cale zdanie ale potrzebna jest petla
 
     System.out.println("Int : " + i);
     System.out.println("Double : " + d);
     System.out.println("Short : " + s);
+    System.out.println("String : " + str);
     //System.out.println("String : " + str);
-    System.out.println("String : " + str1);
 
     in.close();
   }
