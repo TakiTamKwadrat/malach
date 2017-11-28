0ed13e4c4791cd01ed2c79b76fcea65a76822cf8
FridenotChlodulf <Fridenot@mail.com>
2017-11-28 17:41:39 +0100

    ramka

---
+++ ramka/Main.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        JFrame ramka = new JFrame("Test");
+        ramka.setSize(400,500);
+        ramka.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ramka.setLocationRelativeTo(null);
+        ramka.setVisible(true);
+    }
+}
